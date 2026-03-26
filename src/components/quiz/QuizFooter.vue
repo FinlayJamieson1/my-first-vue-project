9df94b5b15bf3b7dc3f2de8af22c4502a47aa7aa
finlayjamieson <finlay.jamieson@wealthatwork.co.uk>
2026-03-26 13:48:33 +0000

    feat: add quiz components and stores

---
+++ src/components/quiz/QuizFooter.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import QuizFooterLinks from '@/components/quiz/QuizFooterLinks.vue'
+
+</script>
+
+<template>
+  <footer>
+    <h4>Quiz Footer</h4>
+    <QuizFooterLinks />
+  </footer>
+</template>
